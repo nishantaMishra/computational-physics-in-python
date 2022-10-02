ec137f7cdef0712f52c6d1ceb7d234d7539f2c41
root <root@kali>
2022-10-03 00:15:45 +0530

    random number generator

---
+++ randomNumberGenerator.py
@@ -0,0 +1,5 @@
+#code for printing a random number between 0 to 20 
+import random 
+for i in range(1):     #here we have called print function multiple(10) times for printing a sequence of random numbers at the same time.
+	print(random.randint(0,20))
+#number must not appear once it  has been generated. 
