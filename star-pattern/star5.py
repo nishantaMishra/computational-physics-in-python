490cbaa500cfb58838279957fc0de56d53fba13e
nishantaMishra <nishunm2000@gmail.com>
2023-01-16 13:52:45 +0530

    added pattern 5

--- star-pattern/star5.py
+++ star-pattern/star5.py
@@ -9,6 +9,8 @@ for i in range(n, -1, -1):  #because step is -1 so we have written in order stop
     for j in range(0, i + 1):
         print("*", end=" ")
     print(" ") # this print function adds new line after the j loop ends. 
+    
+    
  """   
     * * * * * *  
      * * * * *  
