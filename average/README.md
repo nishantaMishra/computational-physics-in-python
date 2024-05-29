43ded6912d54911ad4af780662573be09c2eea38
nishantaMishra <nishunm2000@gmail.com>
2024-05-29 18:19:16 +0530

    definitions added

---
+++ average/README.md
@@ -0,0 +1,10 @@
+# Finding Average (Mean, Median, Mode)
+
+Arithmetic mean, median and mode are the most common measures of central tendency. 
+
+## Mean 
+The most commonly used method foe finding the average of given dataset. To find the mean simply add up all the values and divide by total number of values.
+If given data is $X = \{x_1, x_2, x_3, ... x_n\}$ then mean is
+```math
+\text{mean} = \sum_{i=1}^n \frac{x_i}{n} 
+```
---
+++ average/average.py
@@ -0,0 +1,38 @@
+#Program for finding Centeral Tendencies of the given data set
+
+from collections import Counter
+
+data = [5,2, 6.5, 8,8,4, 3, 22, 3.5, 66,4, 27,8, 2, 4, 0, 2]
+
+#---------------------------------
+data.sort()
+if len(data)%2 == 0:
+    m = (len(data))/2
+    print('Median is',(data[int(m) -1] + data[int(m)])/2) # -1 because indexing starts from zero.
+else:
+    m = (len(data)+1)/2
+    print('Median is', data[int(m) -1 ])
+
+#--------------------------------
+sum = 0
+for i in range(len(data)) :
+    sum += data[i]
+print('Mean is', sum/len(data))
+
+#--------------------------------
+
+c = Counter(data)
+l = c.most_common()
+l.append((0,0))
+for i in range(len(l)-1, 0, -1): #it will start checking data from the last but leaving the first element. 
+    if l[0][1] == 1:
+        print('No mode')
+        break
+    elif l[0][1] == l[i][1] != 1:
+        print('there are more than one modes', end=' - ')
+        for n in range(i+1):
+            print(l[n][0], end=', ')
+        break    
+    elif l[0][1] != l[1][1]:
+        print('The mode is',l[0][0])
+        break
